d02b26af963663795281944b8b6f141302b4917d
Dilshad Abdu Samad <dlz611606@gmail.com>
2023-09-01 18:06:59 +0530

    external-database

--- BankData/BankData/settings.py
+++ BankData/BankData/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 from django.conf import settings
 from datetime import timedelta
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'bankapi',
     "accounts",
-    
+
 
 ]
 
@@ -82,7 +84,7 @@ WSGI_APPLICATION = "BankData.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # DATABASES = {
-    
+
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'Bankapidb',
@@ -93,11 +95,15 @@ WSGI_APPLICATION = "BankData.wsgi.application"
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -139,7 +145,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # }
 
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -156,8 +161,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
